843841be390aa59e1f7b476a5d9034b84039cbd7
Dieter Bingemann <dbingema@users.noreply.github.com>
2021-03-12 11:00:34 +0100

    Ein paar Ideen zu den Klassen und was die so machen sollen...
    ... und einen moeglichen Spiel Ablauf. So als moeglichen Anfang... Braucht noch etwas mehr Arbeit zu den Details...

--- main.py
+++ main.py
@@ -21,6 +21,64 @@ FPS = 60
 # - look for stalemate / draw
 
 
+
+# Noch so ein paar Ideen...
+#
+# Game Klasse
+# - koordiniert den Ablauf
+# - zum Beispiel die Uhr
+# - oder die Maus Klicks
+# - ruft dann die Animationen auf
+# - baut ein neues Spiel auf
+# - startet das Spiel (die Uhr)
+# - ruft den Computer auf einen Zug zu machen
+#
+#
+# Board Klasse
+# - weiss wo alle Figuren stehen
+# - und welche noch da sind
+# - schaut auch welche der erlaubten Züge noch möglich sind (da da keine anderen Figuren stehen)
+# - kann auch entscheiden. ob ein Maus Klick auf einem bestimmten Pixel ein e Figure trifft und wen ja, welche
+#
+# Piece Klasse
+# - alles, was alle Figuren gemeinsam haben
+# - so als eine Art Platzhalter fuer spaeter
+# - wissen wo sie stehen
+# - haben eine Liste mit erlaubten Zügen (relativ zu ihrem Standort)
+# - aber man muss auch Besonderheiten erlauben, wie zum Beispiel den ersten Zug vom Bauern... Oder die Rochade... Oder en passant...
+# - koennen auch sagen, auf welche Felder sie von ihren Standort aus gehen können (geben eine Liste zurueck)
+# - sind schwarz oder weiss
+# - haben ein Bild
+# - koennen sich bewegen von A nach B mit einem netten huebsch aussehenden coolen Move
+# - koennen explodieren, wenn sie geschlagen werden
+#
+# Einzelne Figuren Klassen
+# - besetzen die Platzhalter in "Piece" entsprechend
+# - zum Beispiel, welche spezifischen Zuege der Bauer machen kann - alle Bauern haben die gleichen Zuege - das ist so eine Art Klassen Variable, fuer alle gleich
+# - manche Platzhalter von "Piece" werden erst mit den eigentlichen "Instanzen" besetzt (zum Beispiel wo sie stehen), das ist von Bauer zu Bauer verschieden
+#
+#
+# - zum Beispiel, das Spiel "sieht" wohin die Maus geklickt hat - fragt das Board, ob das eine Figur ist. 
+# - wenn ja, dann Sagt das Spiel dem Board, dass diese Figur bewegt werden soll
+# - Board fragt dann die Figur, wohin sie denn so laufen kann
+# - Board checked dann welche von den legalen Felder denn so moeglich sind (da dort noch nix steht)
+# - das gibt eine kuerzere Liste von moeglichen Feldern
+# - sagt dann dem Spiel, wohin die Figur laufen kann (und gibt dem Spiel auch die Figur fuer drag and drop)
+# - Spiel malt dann die Punkte auf die Felder, wohin die Figur eigentlich nur hin laufen kann
+# - Spiel schaut dann, wohin der Spieler zum zweiten mal clickt - oder drag and drop macht
+# - wenn drag and drop, dann muss das Spiel die Figur mit bewegen
+# - wenn der zweite Klick oder der Drag and Drop auf einem Punkt-Feld landet, dann ruft das Spiel die Animation auf
+# - Spiel sagt der Figur, sich von A nach B zu bewegen
+# - Figure macht das dann
+# - Spiel sagt dann dem Board, dass die Figur nun auf dem neuen Platz steht
+# - Board checked ob das eine andere Figur geschlagen hat
+# - sagt dies dem Spiel dann, falss dem so ist
+# - Spiel sagt dann der geschlagenen Figur doch bitte zu explodieren
+#
+# oder so aehnlich?
+
+
+
 class Board:
     def __init__(self):
         pass
